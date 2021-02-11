40a22eec2cc079dc619d1fb3243f5ce100a89771
shine2 <elina.shin@emc.com>
2021-02-12 02:02:19 +0300

    initial

---
+++ src/main/java/com/ehlina/Main.java
@@ -0,0 +1,8 @@
+package com.ehlina;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello");
+        throw new IllegalArgumentException();
+    }
+}
